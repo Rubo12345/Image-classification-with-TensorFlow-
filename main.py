dc1e6ecd2e7203b6426d13b3e15aa23393ab01be
Rutwik <rutwik.bonde17@vit.edu>
2022-03-22 06:25:18 +0530

    Preprocessing and Normalization

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 from tensorflow.keras.datasets import mnist
@@ -25,3 +26,30 @@ y_test_one_hot = to_categorical(y_test)    #One hot encoding
 
 # print(y_train_one_hot[0])
 # print(y_test_one_hot[0])
+
+x_train_reshape = np.reshape(x_train,(60000,784))  # Converting N -dimensional arrays into vectors
+x_test_reshape = np.reshape(x_test,(10000,784))    # Converting N -dimensional arrays into vectors
+
+# print(x_train_reshape.shape)
+# print(x_test_reshape.shape)
+
+# print(set(x_train_reshape[0]))   # pixel values
+
+'''
+Data Normalization for better computtion and flow.
+Finding mean and standard deviation.
+Initializing epsilon
+x_train_norm = (x_train - x_mean) / (x_std + epsilon)
+why epsilon?
+To avoid very low values of x_std which might cause disturbance in the data
+It is a standard practice during preprocessing
+'''
+
+x_mean = np.mean(x_train_reshape)
+x_std = np.std(x_train_reshape)
+epsilon = 1e-10
+x_train_norm = (x_train_reshape - x_mean)/(x_std + epsilon)
+x_test_norm = (x_test_reshape - x_mean)/(x_std + epsilon)
+
+# print(x_train_norm[0])
+# print(x_test_norm[0])
